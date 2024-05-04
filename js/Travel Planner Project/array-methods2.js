a0533aef80a0531bfada5e352e94e1e7e1fba590
Avi Israeli <aviv8710@gmail.com>
2024-05-04 16:15:28 +0300

    array methods 2

---
+++ js/Travel Planner Project/array-methods2.js
@@ -0,0 +1,98 @@
+let travelData = {
+  destinations: ["Paris", "London", "New York", "Tokyo", "Sydney"],
+  flights: [
+    { flightNumber: "AA123", destination: "New York", duration: "8h 15m" },
+    { flightNumber: "BA456", destination: "London", duration: "7h 30m" },
+    { flightNumber: "AF789", destination: "Paris", duration: "9h 20m" },
+    { flightNumber: "JL012", destination: "Tokyo", duration: "13h 45m" },
+    { flightNumber: "QF345", destination: "Sydney", duration: "22h 35m" },
+  ],
+};
+
+
+
+// 1
+
+const destinationArr = ["New York", "London", "Paris"];
+const newArr = [...destinationArr];
+console.log(newArr);
+
+// 2
+function filterDestination(arr, dest) {
+  const chosenDestination = arr.flights.filter((destination) => dest === destination.destination)
+  return chosenDestination
+}
+
+console.log(filterDestination(travelData, "London")); 
+
+// 3
+
+const getFlightDurationInSeconds = duration => {
+  const [hours, minutes] = duration.split("h ");
+  return parseInt(hours) * 3600 + parseInt(minutes) * 60;
+  };
+ 
+function durationSum(arr) {
+  let secondsArr = [];
+  for (destination of arr.flights) {
+    const durationInSeconds = getFlightDurationInSeconds(destination.duration);
+    secondsArr.push(durationInSeconds);
+  }
+  const sum = secondsArr.reduce((acc, cur) => acc + cur, 0)
+  return sum;
+}
+
+console.log(durationSum(travelData));
+
+// 4
+
+function flightsNums(arr) {
+  const numberAndDestinationArr = arr.flights.map((flight) => `${flight.flightNumber} - ${flight.destination}`);
+  return numberAndDestinationArr;
+}
+
+console.log(flightsNums(travelData));
+
+// 5
+
+function updateDuration(arr, newDuration, flightNumber) {
+  for (flight of arr.flights) {
+    if (flight.flightNumber === flightNumber) {
+      flight.duration = newDuration;
+      return flight;
+    }
+  }
+}
+
+console.log(updateDuration(travelData, newDuration = "4h 15m", flightNumber = "AA123")); 
+
+// 6
+
+function minDurationFilter(arr, minDuration) {
+  const adjustedDurationArr = arr.flights.filter((flight) => getFlightDurationInSeconds(flight.duration) >= minDuration)
+  return adjustedDurationArr
+}
+
+console.log(minDurationFilter(travelData, 30000)); 
+
+// 7
+function flightNumbers(obj) {
+  const flightNumberArr = obj.flights.map((flight) => flight.flightNumber);
+  return flightNumberArr;
+}
+
+console.log(flightNumbers(travelData));
+
+// 8
+
+function sortByDuration(obj) {
+  const sortedByDuration = obj.flights.sort((a, b) => getFlightDurationInSeconds(b.duration) - getFlightDurationInSeconds(a.duration));
+  return sortedByDuration;
+}
+
+console.log(sortByDuration(travelData));
+
+// 9
+
+const destinationNames = travelData.flights.map((flight) => flight.destination)
+console.log(destinationNames);
---
+++ js/Travel Planner Project/objects2.js
@@ -0,0 +1,123 @@
+// 1
+
+const travelPlaner = {
+  locations: [
+    {
+      name: 'Ko phangan',
+      description: 'A place in Thailand',
+      bestTimeToTravel: 'Summer',
+      localCuisine: 'asian food'
+    }, {
+      name: 'Rodus',
+      destination: 'An island in greece',
+      bestTimeToTravel: 'Spring',
+      localCuisine: 'Greece food'
+    }, {
+      name: 'Paris',
+      description: 'Most romantic city',
+      bestTimeToTravel: 'Winter',
+      localCuisine: 'French food'
+    }, {
+      name: 'New York',
+      description: 'The big apple',
+      bestTimeToTravel: 'Fall',
+      localCuisine: 'Pizza'
+    }
+  ],
+  addLocation(nameO, info) {
+    let isName = false;
+    // for every location in the locations arr in the object
+    for (let i = 0; i < this.locations.length; i++) {
+      // check if the location.name === nameO
+      if (this.locations[i].name === nameO) {
+        // if true - clg 'err' and stop the loop
+        console.log("err");
+        break
+      } else {
+        isName = true;
+      }
+    };
+    // now check if isName if true of false
+    if (isName) {
+      // if it's true - push info the the end of locations arr
+      this.locations.push(info)
+    }
+  },
+  deleteLocation(nameA) {
+    let isNameEqual = true;
+    // Iterate over the locations arr, for every location in the arr
+    for (let i = 0; i < this.locations.length; i++) {
+      // check if location.name === name
+      if (this.locations[i].name === nameA) {
+        // if true - delete the specific location from the arr and stop the loop
+        this.locations.splice(i, 1)
+        break
+      } else {
+        // if false - set isNameEqual to false and keep looping
+        isNameEqual = false;
+        break
+      }
+    };
+    // now check if isNameEqual is true or false
+    if (!isNameEqual) {
+      // if it's false - clg 'err'
+      console.log('err');
+    }
+  },
+  findLocation(locationName) {
+    // initialize a boolian to determine if the location was found
+    let isFound = true;
+    // Iterate over the locations arr, for every location in the arr
+    for (let i = 0; i < this.locations.length; i++) {
+      // check if location.name === name
+      if (this.locations[i].name === locationName) {
+        // if true - return the location and stop looping
+        return this.locations[i];
+      } else {
+        // if false - set isFound to false and keep looping
+        isFound = false;
+      }
+    };
+    if (!isFound) {
+      console.log('Location not found');
+    }
+  },
+  sortLocations() {
+    const sorted = this.locations.sort((a, b) => {
+      const nameA = a.name.toLowerCase();
+      const nameB = b.name.toLowerCase();
+      if (nameA < nameB) return -1;
+      if (nameA > nameB) return 1;
+      return 0;
+  }
+);
+return sorted;
+  }
+
+}
+
+// 2
+
+travelPlaner.addLocation('Ko phangan', {name: 'Ko hangan',
+description: 'A place in Thailand',
+bestTimeToTravel: 'Summer',
+localCuisine: 'asian food'})
+
+console.log(travelPlaner);
+
+// 3
+
+travelPlaner.deleteLocation('Ko phangan')
+
+console.log(travelPlaner);
+
+// 4
+
+console.log(travelPlaner.findLocation('Rodus')); 
+
+// 5
+
+console.log(travelPlaner.sortLocations());
+
+// 6
+
