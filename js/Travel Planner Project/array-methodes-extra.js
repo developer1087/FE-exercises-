2fe703df18620cc82a6bd2bf39503315dbb7178b
Avi Israeli <aviv8710@gmail.com>
2024-05-13 14:55:41 +0300

    array methodes extra

--- js/Travel Planner Project/array-methodes-extra.js
+++ js/Travel Planner Project/array-methodes-extra.js
@@ -15,4 +15,61 @@ function groupByDestination(tripsArr) {
   console.log(tripsObj);
 }
 
-groupByDestination(trips)
+// groupByDestination(trips)
+
+// 2
+
+function longestTrip(tripsArr) {
+  const longestTrip = tripsArr.reduce((prev, curr) => (prev && prev.days > curr.days) ? prev : curr)
+  return longestTrip;
+}
+
+console.log(longestTrip(trips));
+
+// 3
+
+function expensiveTrip(tripsArr) {
+  const expensiveTrip = tripsArr.sort((a, b) => b.cost - a.cost)[0]
+  return expensiveTrip;
+} 
+
+console.log(expensiveTrip(trips));
+
+// 4
+
+console.log(trips);
+
+function countTravels(tripsArr) {
+  const travelsCounterObj = {};
+  for (const travel of tripsArr) {
+    if (!(travel.traveler in travelsCounterObj)) {
+      travelsCounterObj[travel.traveler] = 1;
+    } else {
+      travelsCounterObj[travel.traveler]++
+    }
+  }
+  return travelsCounterObj;
+}
+
+console.log(countTravels(trips));
+
+// 5
+
+// function uniqueDestinations(tripsArr) {
+//   let uniqueDestinationsArr = [];
+//   for (const destination of tripsArr) {
+//     if (!(uniqueDestinationsArr.includes(destination.destination))) {
+//       uniqueDestinationsArr.push(destination.destination);
+//     }
+//   }
+//   return uniqueDestinationsArr;
+// }
+
+// console.log(uniqueDestinations(trips));
+
+function uniqueDestinations(tripsArr) {
+  return [...new Set(tripsArr.map(trip => trip.destination))]
+}
+
+console.log(uniqueDestinations(trips));
+
